bafc8ac4cc7b8e609ac7616d4275c46295a5dcb5
Marquiste Boyce <mboyce84@gmail.com>
2014-08-25 15:15:58 -0400

    fixed issue with tab spacing

--- tic_tac_toe.rb
+++ tic_tac_toe.rb
@@ -5,46 +5,46 @@
 
 #METHODS BEGIN#
 def initialize_board
-	p = {}
-	(1..9).each {|position| p[position] = ' '}
-	p
+  p = {}
+  (1..9).each {|position| p[position] = ' '}
+  p
 end
 
 def draw_board(p)
-	system('cls')
-	puts "Welcome to Tic Tac Toe Game"
-	puts "\n"
-	puts "  #{p[1]} | #{p[2]} | #{p[3]} "
-	puts " -----------"
-	puts "  #{p[4]} | #{p[5]} | #{p[6]} "
-	puts " -----------"
-	puts "  #{p[7]} | #{p[8]} | #{p[9]} "
+  system 'clear'
+  puts "Welcome to Tic Tac Toe Game"
+  puts "\n"
+  puts "  #{p[1]} | #{p[2]} | #{p[3]} "
+  puts " -----------"
+  puts "  #{p[4]} | #{p[5]} | #{p[6]} "
+  puts " -----------"
+  puts "  #{p[7]} | #{p[8]} | #{p[9]} "
 end
 
 def empty_positions(p)
-	p.select {|k,v| v == ' ' }.keys
+  p.select {|k,v| v == ' ' }.keys
 end
 
 def check_free_positions(p, user_position)
-	if p.select {|k,v| v != ' ' }.keys.include?(user_position)
-		print "Sorry, but this position is already taken. Please choose again > "
-		user_position = gets.chomp.to_i
-		check_free_positions(p, user_position)
-	else 
-		p[user_position] = 'X'
-	end
+  if p.select {|k,v| v != ' ' }.keys.include?(user_position)
+    print "Sorry, but this position is already taken. Please choose again > "
+    user_position = gets.chomp.to_i
+    check_free_positions(p, user_position)
+  else 
+    p[user_position] = 'X'
+  end
 end
 
 def player_picks_square(p)
-	puts "\nYou are Xs and the Computer is Os"
-	print "\nChoose a position on the board (1 - 9) > "
-	user_position = gets.chomp.to_i
-	position_check = check_free_positions(p, user_position)
+  puts "\nYou are Xs and the Computer is Os"
+  print "\nChoose a position on the board (1 - 9) > "
+  user_position = gets.chomp.to_i
+  position_check = check_free_positions(p, user_position)
 end
 
 def computer_picks_square(p)
-	position = empty_positions(p).sample
-	p[position] = 'O'
+  position = empty_positions(p).sample
+  p[position] = 'O'
 end
 
 def check_winner(p)
@@ -57,16 +57,16 @@ def check_winner(p)
 end
 
 def replay(play_again_choice)
-	if play_again_choice == 'y' || play_again_choice == 'yes'
-		puts "\n---------Time for a rematch!---------\n"
-	elsif play_again_choice == 'n' || play_again_choice == 'no'
-		puts "\n---------Thanks for playing!---------\n"
-		exit
-	else
-		puts "\nUmm, didn't catch that. Would You Like to Play again? (Y/N)"
-		play_again_choice = gets.chomp.downcase
-		replay(play_again_choice)
-	end
+  if play_again_choice == 'y' || play_again_choice == 'yes'
+    puts "\n---------Time for a rematch!---------\n"
+  elsif play_again_choice == 'n' || play_again_choice == 'no'
+    puts "\n---------Thanks for playing!---------\n"
+    exit
+  else
+    puts "\nUmm, didn't catch that. Would You Like to Play again? (Y/N)"
+    play_again_choice = gets.chomp.downcase
+    replay(play_again_choice)
+  end
 end
 #METHODS END#
 
@@ -76,19 +76,19 @@ loop do
 board = initialize_board
 draw_board(board)
 
-begin	
-	player_picks_square(board)
-	computer_picks_square(board)
-	draw_board(board)
-	winner = check_winner(board)
+begin 
+  player_picks_square(board)
+  computer_picks_square(board)
+  draw_board(board)
+  winner = check_winner(board)
 end until winner || empty_positions(board).empty?
 
 if winner == 'Player'
-	puts "\nYou won!"
+  puts "\nYou won!"
 elsif winner == 'Computer'
-	puts "\nThe computer won!"
+  puts "\nThe computer won!"
 else
-	puts "\nIt's a tie!"
+  puts "\nIt's a tie!"
 end
 
 print "\nWould you like to play another round of Tic-Tac-Toe? (Y/N) > "
